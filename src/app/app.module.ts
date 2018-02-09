4f0f2e5b3b83c1fc9123a654758e7b55bfecf788
GPC Admin <gpcadmin@GPCs-Mac-mini.local>
2018-02-09 10:22:05 -0500

    added order component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
